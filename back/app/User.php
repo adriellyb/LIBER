297f8ffcc83936144661dc6507ab7b95c5a6516c
adriellyb <adrielly0195@gmail.com>
2021-01-30 16:46:08 -0300

    +alterando a model de usuarios

--- back/app/User.php
+++ back/app/User.php
@@ -6,6 +6,26 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class User extends Model
+{
+    public function createUser(Request $request){
+        $this->name = $request->name;
+        $this->email = $request->email;
+        $this->password = $request->password;
+        $this->cpf = $request->cpf;
+        $this->phone = $request->phone;
+        $this->address = $request->address;
+        $this->number = $request->number;
+        $this->complement = $request->complement;
+        $this->district = $request->district;
+        $this->city = $request->city;
+        $this->save();
+    }
+}
+
 class User extends Authenticatable
 {
     use Notifiable;
