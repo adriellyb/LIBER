6fa69450ac838ea0e68f129fbfa8289cd37f429b
adriellyb <adrielly0195@gmail.com>
2021-01-30 16:45:33 -0300

    +adicionando atributos a migration de users

--- back/database/migrations/2014_10_12_000000_create_users_table.php
+++ back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,15 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cpf');
+            $table->string('phone');
+            $table->string('address');
+            $table->integer('number');
+            $table->string('complement');
+            $table->string('district');
+            $table->string('city');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
