1d8cc6f6e553aa9ace03f7f568268646fb6da92a
adriellyb <adrielly0195@gmail.com>
2021-01-30 16:48:21 -0300

    +add rotas da BookController com métodos Http

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/** Rota para ser ululizada no Postman: http://localhost:8000/api/books */
+
+/** Rotas para as funções criadas na BookController */
+Route::get('books',[BookController::class, 'index']);
+
+Route::get('books/{id}',[BookController::class, 'show']);
+
+Route::post('books',[BookController::class, 'create']);
+
+Route::put('books/{id}',[BookController::class, 'update']);
+
+Route::delete('books/{id}',[BookController::class, 'destroy']);
